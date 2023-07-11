51ca7d3bcf68b6d3afeaffc14d21d024e1125871
Brett Cannon <brett@python.org>
2023-07-10 17:16:13 -0700

    Use Rich to provide a progress bar for downloading `pip.pyz`

--- src/py_pip/download.py
+++ src/py_pip/download.py
@@ -1,9 +1,24 @@
 import httpx
+import rich.progress
 
 
 PYZ_URL = "https://bootstrap.pypa.io/pip/pip.pyz"
 
+
 def download_pyz() -> bytes:
-    response = httpx.get(PYZ_URL, follow_redirects=True)
+    with httpx.stream("GET", PYZ_URL) as response:
+        content = []
+        total = int(response.headers["Content-Length"])
+
+        with rich.progress.Progress(
+            "[progress.percentage]{task.percentage:>3.0f}%",
+            rich.progress.BarColumn(bar_width=None),
+            rich.progress.DownloadColumn(),
+            rich.progress.TransferSpeedColumn(),
+        ) as progress:
+            download_task = progress.add_task("Download", total=total)
+            for chunk in response.iter_bytes():
+                content.append(chunk)
+                progress.update(download_task, completed=response.num_bytes_downloaded)
     response.raise_for_status()
-    return response.content
+    return b"".join(content)
