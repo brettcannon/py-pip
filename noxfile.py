722e37036b59d68fd775ce402e7363efee82640f
Brett Cannon <brett@python.org>
2023-07-10 17:15:57 -0700

    Switch `noxfile.py` to create a minimum Python virtual environment

--- noxfile.py
+++ noxfile.py
@@ -1,11 +1,35 @@
-import nox # type: ignore
+import os.path
+import pathlib
+import shutil
+import tomllib
 
-@nox.session
-def lock(session: nox.Session) -> None:
-    session.install("pip-tools")
-    session.run("pip-compile", "--generate-hashes", "--resolver=backtracking", "requirements.in")
+import nox  # type: ignore
+
+WORKSPACE = pathlib.Path(__file__).parent
 
 
-@nox.session(python=["3.11", "3.10", "3.9", "3.8", "3.7"])
-def env(session: nox.Session) -> None:
-    session.install("--require-hashes", "--only-binary=:all:", "--no-deps", "-r", "requirements.txt")
+@nox.session(python=False)
+def venv(session: nox.Session):
+    """Create a virtual environment."""
+    venv_path = WORKSPACE / ".venv"
+    if venv_path.exists():
+        shutil.rmtree(venv_path)
+    with open(WORKSPACE / "pyproject.toml", "rb") as file:
+        pyproject = tomllib.load(file)
+    min_version = pyproject["project"]["requires-python"].removeprefix(">=").strip()
+    session.run("py", f"-{min_version}", "-m", "venv", venv_path)
+    site_packages = venv_path / "lib" / f"python{min_version}" / "site-packages"
+    session.run(
+        "py",
+        "-m",
+        "pip",
+        "install",
+        f"--target={site_packages}",
+        "--implementation=py",
+        f"--python-version={min_version}",
+        "--abi=none",
+        "--platform=any",
+        "--only-binary=:all:",
+        "-e",
+        ".",
+    )
